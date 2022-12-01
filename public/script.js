a220cf99d1333b3618b1a42bdb09f093591c2b9e
RAMJAN <ramjanali261@gmail.com>
2022-12-02 01:13:25 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,150 @@
+const socket=io('/');
+const videoGrid=document.getElementById('video-grid');
+const myVideo=document.createElement('video');
+myVideo.muted=true;
+
+//try
+
+// var userName=prompt("Enter name: ");
+//     //   console.log(userName)
+//       var span = document.getElementById('na');
+//       span.innerText = span.textContent = `${userName}`;
+      
+
+//new peer connection
+var peer= new Peer(undefined,{
+    path: '/peerjs',
+    host:'/',
+    port:'3000'
+});
+
+let myVideoStream
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream =>{
+    myVideoStream=stream;
+    addVideoStream(myVideo,stream);
+
+    peer.on('call',call =>{
+        call.answer(stream)
+        const video=document.createElement('video')
+        call.on('stream',userVideoStream=>{
+            addVideoStream ( video,userVideoStream)
+        })
+    })
+
+    socket.on('user-connected',(userId)=>{
+        connecToNewUser(userId,stream); //defined the function below
+    })    
+
+    //message 
+    let text = $('input')
+
+$('html').keydown((e)=>{
+    if(e.which == 13 && text.val().length !==0) {
+        socket.emit('message', text.val());
+        text.val('')
+    }
+})
+
+socket.on('createMessage',message =>{
+    $('.messages').append(`<li class="message"><b>user</b><br/>${message}</li><br/>`)
+    scrollToBottom()
+})
+
+})
+
+//open the peer room
+peer.on('open',id =>{
+    // console.log(id);
+    socket.emit('join-room',ROOM_ID,id);
+    
+})
+
+//function code connect to user
+const connecToNewUser=(userId,stream)=>{
+    // console.log(userId); //client sent the user id
+    const call=peer.call(userId,stream)
+    const video=document.createElement('video')
+    call.on('stream',userVideoStream =>{
+        addVideoStream(video,userVideoStream)
+    })
+}
+
+
+const addVideoStream = (video,stream) =>{
+    video.srcObject=stream;
+    video.addEventListener('loadedmetadata',()=>{
+        video.play();
+    })
+    videoGrid.append(video);
+}
+
+//window size scroll
+const scrollToBottom= ()=>{
+    let d=$('.main_chat_window');
+    d.scrollTop(d.prop("scrollheight"));
+}
+
+const scrollToBot= ()=>{
+    let d=$('.chat-area');
+    d.scrollTop(d.prop("scrollheight"));
+}
+//message
+const sendmes= ()=>{
+    var msg = document.getElementById("chat_message").value;
+    if(msg.length !==0) {
+        socket.emit('message', msg);
+    }
+
+}
+
+//mute our video
+const muteUnmute= ()=>{
+    const enabled= myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getAudioTracks()[0].enabled=false;
+        setUnmuteButton();
+    }else{
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled=true;
+    }
+}
+
+const setMuteButton =()=>{
+    const html=`<i class="fa fa-microphone"></i>`
+
+    document.querySelector('.main_mute_button').innerHTML=html;
+}
+
+const setUnmuteButton =()=>{
+    const html=`<i class="unmute fa fa-microphone-slash"></i>`
+
+    document.querySelector('.main_mute_button').innerHTML=html;
+}
+
+//stop video
+const playStop=()=>{
+    // console.log('object')
+    let enabled=myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getVideoTracks()[0].enabled=false;
+        setPlayVideo()
+    }else{
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled=true;
+    }
+}
+
+const setStopVideo =()=>{
+    const html=`<i class=" fa fa-video"></i>`
+
+    document.querySelector('.main_video_button').innerHTML=html;
+}
+
+const setPlayVideo =()=>{
+    const html=`<i class="stopp fa fa-video-slash"></i>`
+
+    document.querySelector('.main_video_button').innerHTML=html;
+}
---
+++ server.js
@@ -0,0 +1,58 @@
+const express= require('express');
+const app= express();
+const server=require('http').Server(app);
+
+const io =require('socket.io')(server)
+
+const {v4: uuidv4}=require('uuid');
+
+const port=process.env.PORT || 3000;
+
+//peer library import
+const { ExpressPeerServer } = require('peer');
+const peerServer=ExpressPeerServer(server,{
+    debug:true
+});
+
+//setting view engine(ejs)
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+
+app.use('/peerjs',peerServer);
+//setting the unique id in the url
+//--redirecting the home page to room-->with uuid parameter
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.get('/room_ui',(req,res)=>{
+    res.redirect(`/room_ui${uuidv4()}`);
+})
+
+//setting the unique id in the url
+//--redirecting the home page to room-->with uuid parameter
+//setting the room id to web address
+app.get('/:room_ui',(req,res)=>{ 
+    res.render('room_ui',{roomId: req.params.room})
+})
+
+//socket connection for joining room
+io.on('connection',socket =>{
+    socket.on('join-room', (roomId,userId)=>{
+        // console.log("joined room");
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected',userId);
+        // socket.to(roomId).broadcast.emit('user-connected'); //error code 
+
+        socket.on('message',message => {
+            io.to(roomId).emit('createMessage',message)
+        })
+    })
+})
+
+
+server.listen(port, ()=>{
+    console.log(`started port ${port}`)
+});
